2da4c60972ab721952e2d4c0738bae08751fbdeb
Phimnada CHIRACHOTSUPHAPHAT <phimnada.c@ku.th>
2024-08-24 21:52:02 +0700

    Write docstring in models.py

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,6 @@
+"""
+This module defines model-related classes for polls application
+"""
 import datetime
 
 from django.contrib import admin
@@ -6,10 +9,17 @@ from django.utils import timezone
 
 
 class Question(models.Model):
+    """
+    Represents a poll question in the application.
+    Each question has its own text and a publication date.
+    """
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
+        """
+        Returns a string representation of the question text.
+        """
         return self.question_text
 
     @admin.display(
@@ -18,14 +28,24 @@ class Question(models.Model):
         description='Published recently?',
     )
     def was_published_recently(self):
+        """
+        Returns True if the question was published within the last day.
+        """
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
+    """
+    Represents a choice in a poll question.
+    Each choice is a part of  a specific question and has its own text and vote count.
+    """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
+        """
+        Returns a string representation of the choice text.
+        """
         return self.choice_text
