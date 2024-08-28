4be07ef7cadca71df0108ed0d44f36ef93fe9823
Phimnada CHIRACHOTSUPHAPHAT <phimnada.c@ku.th>
2024-08-28 21:06:50 +0700

    Modify to use is_published and can_vote in views.py

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.utils import timezone
 from django.views import generic
 from .models import Choice, Question
 
@@ -14,7 +13,8 @@ class IndexView(generic.ListView):
         """
         Return the last five published questions (not including those set to be published in the future).
         """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        published_question_list = [q.pk for q in Question.objects.all() if q.is_published()]
+        return Question.objects.filter(pk__in=published_question_list).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -25,16 +25,29 @@ class DetailView(generic.DetailView):
         """
         Excludes any questions that aren't published yet.
         """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+        published_question_list = [q.pk for q in Question.objects.all() if q.is_published()]
+        return Question.objects.filter(pk__in=published_question_list)
 
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        published_question_list = [q.pk for q in Question.objects.all() if q.is_published()]
+        return Question.objects.filter(pk__in=published_question_list)
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    if not question.can_vote():
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "Voting is not allowed for this question.",
+        })
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
