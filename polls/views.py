dd57ce890582c548acd054253f6d12ee7d5ae9a5
Phimnada CHIRACHOTSUPHAPHAT <phimnada.c@ku.th>
2024-09-05 23:11:48 +0700

    Log the events using Python Logging

--- polls/views.py
+++ polls/views.py
@@ -8,6 +8,9 @@ from django.urls import reverse
 from django.views import generic
 from django.contrib.auth.decorators import login_required
 from .models import Choice, Question, Vote
+from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
+from django.dispatch import receiver
+import logging
 
 
 class IndexView(generic.ListView):
@@ -95,30 +98,44 @@ class ResultsView(generic.DetailView):
         return Question.objects.filter(pk__in=published_question_list)
 
 
+def get_client_ip(request):
+    """Get the visitor’s IP address using request headers."""
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+logger = logging.getLogger('polls')
+
+
 @login_required
 def vote(request, question_id):
     """
     Handle voting for a specific question.
     """
     question = get_object_or_404(Question, pk=question_id)
+    this_user = request.user
+    ip_address = get_client_ip(request)
 
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
+        logger.warning(f"{this_user} failed to vote in {question} from {ip_address}")
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
         })
 
-    # References to the current user
-    this_user = request.user
-
     # Get the user's vote
     try:
-        vote = Vote.objects.get(user=this_user, choice__question=question)
+        user_vote = Vote.objects.get(user=this_user, choice__question=question)
         # user has a vote for this question! Update his choice.
-        vote.choice = selected_choice
-        vote.save()
+        user_vote.choice = selected_choice
+        user_vote.save()
+        logger.info(f'{this_user} voted for Choice {selected_choice.id} in Question {question.id} from {ip_address}')
         messages.success(request, f"Your vote was updated to '{selected_choice.choice_text}'")
     except Vote.DoesNotExist:
         # does not have a vote yet
@@ -128,3 +145,21 @@ def vote(request, question_id):
 
     return HttpResponseRedirect(reverse('polls:results',
                                         args=(question_id,)))
+
+
+@receiver(user_logged_in)
+def log_user_login(request, user, **kwargs):
+    ip_address = get_client_ip(request)
+    logger.info(f'{user} logged in from {ip_address}')
+
+
+@receiver(user_logged_out)
+def log_user_logout(request, user, **kwargs):
+    ip_address = get_client_ip(request)
+    logger.info(f'{user} logged out from {ip_address}')
+
+
+@receiver(user_login_failed)
+def log_user_login_failed(request, **kwargs):
+    ip_address = get_client_ip(request)
+    logger.warning(f'User failed to log in from {ip_address}')
